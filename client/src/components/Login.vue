e787c6bcfb15d4d7adfb4230a1cd334043abe85b
Mayank Jhawar <mayank_jhawar@certree.com>
2021-09-20 01:25:03 +0530

    fix validation

--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -55,13 +55,14 @@ export default {
   name: "Login",
   components: {},
   data: () => ({
+    valid: false,
     auth: {
       username: "",
       password: "",
     },
     demo: {
       username: "admin@admin.com",
-      password: "demo@123",
+      password: "abcaamayank",
     },
     emailRules: [
       (v) => !!v || "E-mail is required",
@@ -69,7 +70,98 @@ export default {
         /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v) ||
         "E-mail must be valid",
     ],
-    passwordRules: [(v) => !!v || "Password is required"],
+    passwordRules: [
+      (v) => !!v || "Password is required",
+      (v) =>
+        !["i", "o", "l"].includes(v) ||
+        "We don't allowed 'i', 'o', 'l' in password",
+      (v) => /^[a-z]+$/.test(v) || "Password only lowercase letters allowed",
+      (v) =>
+        v.toString().length < 32 ||
+        "Password can't be greater than 32 characters",
+      (v) => {
+        const alphabates = [
+          "a",
+          "b",
+          "c",
+          "d",
+          "e",
+          "f",
+          "g",
+          "h",
+          "i",
+          "j",
+          "k",
+          "l",
+          "m",
+          "n",
+          "o",
+          "p",
+          "q",
+          "r",
+          "s",
+          "t",
+          "u",
+          "v",
+          "w",
+          "x",
+          "y",
+          "z",
+        ];
+        for (let i = 0; i < alphabates.length; i++) {
+          const ele = alphabates[i];
+          let str = ele;
+          let next = i + 1;
+          let nextToNext = i + 2;
+          if (i === alphabates.length - 1) {
+            next = 0;
+            nextToNext = 1;
+          }
+          str = str + alphabates[next] + alphabates[nextToNext];
+          if (v.indexOf(str) >= 0) {
+            return true;
+          }
+        }
+        return "Password conatin atleast one incremantal seriece for eg 'abc', 'bcd'";
+      },
+      (v) => {
+        const alphabates = [
+          "a",
+          "b",
+          "c",
+          "d",
+          "e",
+          "f",
+          "g",
+          "h",
+          "i",
+          "j",
+          "k",
+          "l",
+          "m",
+          "n",
+          "o",
+          "p",
+          "q",
+          "r",
+          "s",
+          "t",
+          "u",
+          "v",
+          "w",
+          "x",
+          "y",
+          "z",
+        ];
+        for (let i = 0; i < alphabates.length; i++) {
+          const ele = alphabates[i] + alphabates[i];
+          if (v.indexOf(ele) >= 0) {
+            return true;
+          }
+        }
+        return "Password must conatin atleast one overlapping pair for eg 'aa', 'bb'..";
+      },
+    ],
   }),
   methods: {
     submit() {
